51ca125ca988e3eb9830d1b289c7b09c252b53ec
Shuvo Kumar Saha <65779943+Shuvo01912@users.noreply.github.com>
2023-09-01 00:04:47 +0600

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ACSSi_To_Int_Converter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // Ascii To Int any type of charcher
+            Console.WriteLine("Enter Any Char to Get ASCII Code:");
+            char c = Convert.ToChar(Console.ReadLine());
+            int a = Convert.ToInt32(c);
+            Console.WriteLine(a);
+        }
+    }
+}
